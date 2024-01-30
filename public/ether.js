cb1c9e79451b31efb8e14e56b68ca2f7b053d032
HugoZak <zakrajsekhugo@gmail.com>
2024-01-30 14:41:17 +0100

    ⚡️ fix ether.js

--- public/ether.js
+++ public/ether.js
@@ -1,4 +1,8 @@
-import { ethers } from "./ethers-5.2.esm.min";
+import { ethers } from "./ethers-5.2.esm.min.js";
 
-let randomBytes = ethers.utils.randomBytes(8);
-
+let randomBytes = ethers.utils.randomBytes(16);
+console.log(randomBytes)
+let wordlist = ethers.utils.entropyToMnemonic(randomBytes)
+console.log(wordlist)
+let adress = ethers.Wallet.fromMnemonic(wordlist)
+console.log(adress)
