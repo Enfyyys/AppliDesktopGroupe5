788af751c3357fda9670d85441706611dd07cd18
mdhoury49000 <mathieu.dhoury@activbold.com>
2024-01-30 16:41:35 +0100

    new fetures :zap:

--- public/ether.js
+++ public/ether.js
@@ -1,23 +1,22 @@
 import { ethers } from "./ethers-5.2.esm.min.js";
 
 
-let randomBytes = ethers.utils.randomBytes(randomNumber(16,32));
-console.log(randomBytes)
-let wordlist = ethers.utils.entropyToMnemonic(randomBytes)
-console.log(wordlist)
-let adress = ethers.Wallet.fromMnemonic(wordlist)
-console.log(adress)
-
-// Function pour avoir un nombre aléatoire entre 16 et 32, et qui soit un multiple de 4
-function randomNumber(min, max) {
+// ether.js
+export function generateRandomBytes(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     let result = Math.floor(Math.random() * (max - min + 2)) + min;
-    if(result%4 == 0)
-    {
-        return result;
-    }else{
-        return randomNumber(min, max);
+    if(result % 4 === 0) {
+        return ethers.utils.randomBytes(result);
+    } else {
+        return generateRandomBytes(min, max);
     }
-    
+}
+
+export function generateMnemonic(bytes) {
+    return ethers.utils.entropyToMnemonic(bytes);
+}
+
+export function generateAddress(mnemonic) {
+    return ethers.Wallet.fromMnemonic(mnemonic);
 }
