f830f9c59975ef609525998a9a7ced6d7315215e
mdhoury49000 <mathieu.dhoury@activbold.com>
2024-01-31 09:12:44 +0100

    random :zap:

--- ether.js
+++ ether.js
@@ -2,15 +2,12 @@ import { ethers } from "./ethers-5.2.esm.min.js";
 
 
 // ether.js
-export function generateRandomBytes(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    let result = Math.floor(Math.random() * (max - min + 2)) + min;
-    if(result % 4 === 0) {
-        return ethers.utils.randomBytes(result);
-    } else {
-        return generateRandomBytes(min, max);
-    }
+export function generateRandomBytes() {
+    const values = [16, 20, 24, 28, 32]
+    const randomIndex = Math.floor(Math.random() * values.length);
+    const randomValue = values[randomIndex];
+    console.log(randomValue);
+    return ethers.utils.randomBytes(randomValue);
 }
 
 export function generateMnemonic(bytes) {
